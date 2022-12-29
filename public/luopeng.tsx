e9b85bd5e67b8025c9f649ae0144291c3fa34134
luopeng <1469104767@qq.com>
2022-12-29 13:44:18 +0800

    内容填充

--- public/luopeng.tsx
+++ public/luopeng.tsx
@@ -5,9 +5,14 @@ const user = {
     job:"前端开发",
     tel:"18582484767",
     email:"18582484767@163.com",
-    school:"四川师范大学",
-    major:"软件工程",
+    school:{
+        name:"四川师范大学",
+        major:"软件工程",
+        time:"2014-2018",
+        grade:"本科"
+    },
     skills:[
+        "熟练掌握Html/CSS/Javascript",
         "精通React开发框架、React Hook和React全家桶,擅长使用React Hook",
         "熟练掌握webpack自动化构建工具,使用过vite等自动化构建工具",
         "熟练掌握node,熟悉next和express等node框架",
@@ -15,16 +20,51 @@ const user = {
     ],
     experience:[
         {
+            companyName:"上海微问家信息技术有限公司",
+            time:["2022.05","至今"],
+            itemList:[
+                {
+                    name:"客户中心",
+                    href:"https://kefu.weiwenjia.com/",
+                    technology:["React","TypeScript","AntDesignPro"],
+                    discription:[
+                        "账号中心/机器人实施流程/任务管理等业务模块的开发",
+                        "电话助手等外接话机拨号功能，逸创云知识库外部对接",
+                        "企业微信侧边栏功能开发",
+                    ]
+                },
+                {
+                    name:"工单系统（重构）",
+                    href:"https://lxcrm.weiwenjia.com/",
+                    technology:["React","Craco","LxUI"],
+                    discription:[
+                        "工单系统重构项目搭建",
+                        "工单列表/客服组/自定义字段等业务模块开发",
+                    ]   
+                },
+            ]
+        },
+        {
             companyName:"中天智慧科技有限公司",
             time:["2018 01","2022.03"],
             itemList:[
                 {
                     name:"天府商城",
-                    technology:["React","Node"]
+                    technology:["React","Node"],
+                    discription:[
+                        "商品/订单/购物车等多个模块的业务开发",
+                        "后管平台多个管理模块开发",
+                        "webpack压缩资源"
+                    ]
                 },
                 {
                     name:"商户平台",
-                    technology:["React","Redux","Webpack"]
+                    technology:["React","Redux","Webpack"],
+                    discription:[
+                        "商品/订单/店铺/合同等管理模块的开发",
+                        "店铺/活动页的可视化",
+                        "webpack优化项目,提升性能"
+                    ]
                 },
                 // {
                 //     name:"张家口银行专属手机",
@@ -36,28 +76,25 @@ const user = {
                 // },
                 {
                     name:"四川天府银行小程序",
-                    technology:["React","Redux","Trao"]
+                    technology:["React","Redux","Trao"],
+                    discription:[
+                        "首页/我的/网点等模块的开发",
+                        "支付功能对接微信支付API,网点功能对接百度地图API",
+                        "C端推客/助力活动/合伙人等活动的开发",
+                        "项目搭建,前端任务分发"
+                    ]
                 },
                 {
                     name:"四川天府银行公众号（重构）",
-                    technology:["React","Redux"]
+                    technology:["React","Redux"],
+                    discription:[
+                        "项目搭建，整体功能对接",
+                        "账号管理/余额查询/对公业务的重构工作",
+                    ]
                 }
             ]
-        },
-        {
-            companyName:"上海微问家信息技术有限公司",
-            time:["2022.05","至今"],
-            itemList:[
-                {
-                    name:"客户中心",
-                    technology:["React","TypeScript","AntDesignPro"]
-                },
-                {
-                    name:"工单系统（重构）",
-                    technology:["React","Craco","LxUI"]
-                },
-            ]
         }
+
     ]
 }
 export default user
--- src/pages/mine/index.tsx
+++ src/pages/mine/index.tsx
@@ -1,11 +1,12 @@
 import React, { useEffect, useState } from "react";
 import { PhoneFilled, MailFilled } from "@ant-design/icons";
+import { Tag } from "antd"
 import userInfo from "@public/luopeng";
 import './index.less'
 
 
 const Mine = () => {
-    const { name, sex, birth, job, tel, email, school, major, skills } = userInfo
+    const { name, sex, birth, job, tel, email, school, major, skills, experience } = userInfo
 
     const call = (tel: string) => {
         window.location.href = `tel:${tel}`
@@ -22,7 +23,6 @@ const Mine = () => {
             <div className="userinfo">
                 <div className="name">{name}</div>
                 <div className="reference">{sex}/{birth}/{job}</div>
-                <div className="school marginTop10">{school} {major}</div>
                 <div className="info marginTop10">
                     <div className="tel" onClick={() => call(tel)}><PhoneFilled className="green" />{tel}</div>
                     <div className="email green" onClick={() => mailTo(email)}><MailFilled />{email}</div>
@@ -43,13 +43,57 @@ const Mine = () => {
             {/* 工作经历 */}
             <div className="skill">
                 <div className="title-box">
-                    <div className="title">个人技能</div>
+                    <div className="title">工作经历</div>
                     <div className="line" />
                 </div>
                 <div className="content">
-                    <ul>
-                        {skills?.map((item: any, index: any) => (<li key={index} className="skill-item">{item}</li>))}
-                    </ul>
+                    <div className="exp">
+                        {experience?.map((item: any) => (
+                            <div className="exp-item" key={item?.companyName}>
+                                <div className="exp-title">
+                                    <div className="exp-title-name">
+                                        {item?.companyName}
+                                    </div>
+                                    <div className="exp-title-tile">
+                                        {item?.time[0]}~{item?.time[1]}
+                                    </div>
+                                </div>
+                                <div className="exp-content">
+                                    {item?.itemList?.map((a: any) => (
+                                        <div key={a.name}>
+                                            <div className="exp-content-name">
+                                                {a.name}
+                                            </div>
+                                            <div className="exp-content-technology">
+                                                {a?.technology?.map((b: any) => (<Tag key={b}>{b}</Tag>))}
+                                            </div>
+                                            <ul className="exp-content-discription">
+                                                {a?.discription?.map((b: any) => (<li key={b}>{b}</li>))}
+                                            </ul>
+                                        </div>))}
+                                </div>
+                            </div>))}
+                    </div>
+                </div>
+            </div>
+            {/* 教育经历 */}
+            <div className="school">
+                <div className="title-box">
+                    <div className="title">教育经历</div>
+                    <div className="line" />
+                </div>
+                <div className="content">
+                    <div className="school-info">
+                        <div className="school-info-name">
+                            {school?.name}
+                        </div>
+                        <div className="school-info-time">
+                            {school?.time}
+                        </div>
+                    </div>
+                    <div className="school-major marginTop10">
+                        {school?.grade} - {school?.major}
+                    </div>
                 </div>
             </div>
         </div>)
